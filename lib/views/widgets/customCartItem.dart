0b72591324458044011990577208720177965ad9
israaelnajjar <israaelnajjar664@gmail.com>
2023-08-07 19:55:35 +0300

    initial card page

---
+++ lib/views/widgets/customCartItem.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+
+class customCartItem extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Container(
+
+   );
+  }
+
+}
