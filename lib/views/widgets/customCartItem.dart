d1356d764388061fc923f8f45fce89e1ac2d7dd5
israaelnajjar <israaelnajjar664@gmail.com>
2023-08-07 20:00:49 +0300

    new

--- lib/views/widgets/customCartItem.dart
+++ lib/views/widgets/customCartItem.dart
@@ -5,7 +5,11 @@ class customCartItem extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
    return Container(
+       child: Column(
+         children: [
 
+         ],
+       ),
    );
   }
 
