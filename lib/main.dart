7c727f258d07070c1e98c33d86874a723b461d8e
Omar_Ameer <omar.ameer244@gmail.com>
2023-08-07 20:31:22 +0300

    Splash, onboarding, get started screens done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:groceries_app/screens/get_started_screen.dart';
+import 'package:groceries_app/screens/onboarding_screen.dart';
+import 'package:groceries_app/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,60 +14,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Omar Ameer'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      title: 'Groceries App',
+      routes: {
+        SplashScreen.id: (context) => const SplashScreen(),
+        OnBoardingScreen.id: (context) => OnBoardingScreen(),
+        GetStartedScreen.id: (context) => const GetStartedScreen(),
+      },
+      initialRoute: SplashScreen.id,
     );
   }
 }
